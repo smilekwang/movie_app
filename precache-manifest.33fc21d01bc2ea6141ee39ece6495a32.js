646ab2c8f5bf5dd3abd062adf0c5a9feac830f60
Kwangsu Kim <smilekwang@gmail.com>
2019-04-23 03:13:10 +0900

    Updates

---
+++ precache-manifest.33fc21d01bc2ea6141ee39ece6495a32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "a0aed7dcfdf2b03fd1cc",
+    "url": "/movie_app/static/js/main.c9896e49.chunk.js"
+  },
+  {
+    "revision": "4ff4439962131c47cd59",
+    "url": "/movie_app/static/js/2.ed6b2ec3.chunk.js"
+  },
+  {
+    "revision": "a0aed7dcfdf2b03fd1cc",
+    "url": "/movie_app/static/css/main.ac91316c.chunk.css"
+  },
+  {
+    "revision": "c921d500504ce9f6a852155ac2591d0e",
+    "url": "/movie_app/index.html"
+  }
+];
